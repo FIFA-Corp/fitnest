88b334e28832954b50bd0e2d11929805ba32fadd
Fikri Alwan Ramadhan <fikriar93@gmail.com>
2022-10-15 22:51:52 +0700

    feat: add brand container component

--- src/component/brand-container.tsx
+++ src/component/brand-container.tsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
-import { CategoryType } from "../types/category-type";
+import { BrandType } from "../types/brand-type";
 import { fetcher, useSWR } from "./../libs";
 import { BrandCategoryCard } from "./ui/card";
 
-export default function CategoryContainer() {
+export default function BrandContainer() {
   const { data: categories, error } = useSWR(
-    `${import.meta.env.VITE_BACKEND_API_URL}/categories?$lookup[_id]=*
+    `${import.meta.env.VITE_BACKEND_API_URL}/brands?$lookup[_id]=*
   `,
     fetcher
   );
@@ -22,14 +22,14 @@ export default function CategoryContainer() {
     <div className="w-full py-3 px-11 border-[1px] border-custom-blush-pink">
       <div className="flex justify-between items-center mb-3">
         <h2 className="font-normal text-2xl text-custom-black-primary">
-          Kategori Pilihan
+          Brand Pilihan
         </h2>
         <Link to="/" className="text-custom-blue-secondary font-normal text-xl">
           Lihat Semua
         </Link>
       </div>
       <div className="flex gap-3">
-        {categories.map(({ _id, name, image }: CategoryType) => {
+        {categories.map(({ _id, name, image }: BrandType) => {
           return (
             <BrandCategoryCard key={_id} name={name} imageUrl={image[0]?.url} />
           );
