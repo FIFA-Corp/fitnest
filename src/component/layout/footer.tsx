a9b9705cfabddf4c9f244bc2616a0b0f34eedd0f
Fikri Alwan Ramadhan <fikriar93@gmail.com>
2022-10-16 19:44:37 +0700

    fix: change fifa fitnest to fitnest in footer

--- src/component/layout/footer.tsx
+++ src/component/layout/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="text-center py-2">
-      Copyright © 2022. FIFA FITNEST Toko Perlengkapan Olahraga Terlengkap
+      Copyright © 2022. FITNEST Toko Perlengkapan Olahraga Terlengkap
     </footer>
   );
 }
