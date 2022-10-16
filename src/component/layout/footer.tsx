ff54cbe4b3342795c690e807ce3ac9cd3263b6e1
Fikri Alwan Ramadhan <fikriar93@gmail.com>
2022-10-16 19:42:57 +0700

    feat: add copyright content in footer

--- src/component/layout/footer.tsx
+++ src/component/layout/footer.tsx
@@ -1,3 +1,7 @@
 export default function Footer() {
-  return <footer>Footer code here</footer>;
+  return (
+    <footer className="text-center py-2">
+      Copyright © 2022. FIFA FITNEST Toko Perlengkapan Olahraga Terlengkap
+    </footer>
+  );
 }
