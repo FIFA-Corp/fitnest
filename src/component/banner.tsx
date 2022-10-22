108f349482a097f22c1af8638938fe7135fa315b
Fikri Alwan Ramadhan <fikriar93@gmail.com>
2022-10-22 14:50:19 +0700

    feat: check condition auth when delete cart

--- src/component/banner.tsx
+++ src/component/banner.tsx
@@ -3,7 +3,7 @@ import banner from "./ui/images/banner.png";
 export default function Banner() {
   return (
     <div className=" flex items-center">
-      <img src={banner}></img>
+      <img src={banner} width="100%" height="auto"></img>
     </div>
   );
 }
--- src/services/cart/detele-cart.ts
+++ src/services/cart/detele-cart.ts
@@ -1,5 +1,11 @@
 import axios from "axios";
+import { getHeaders } from "../../libs/headers";
 
 export const deleteCart = async (cartId: string) => {
-  await axios.delete(`${import.meta.env.VITE_BACKEND_API_URL}/carts/${cartId}`);
+  await axios.delete(
+    `${import.meta.env.VITE_BACKEND_API_URL}/carts/${cartId}`,
+    {
+      headers: getHeaders(),
+    }
+  );
 };
