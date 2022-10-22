7f3c4abb038b18a083323bdaf0616030ba7efa43
Agung Nur Ramadhan <agnuramdan@gmail.com>
2022-10-22 17:41:42 +0700

    chore: change position of the back button

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -51,73 +51,75 @@ export default function Login() {
   };
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-custom-white  p-4">
-      <form
-        onSubmit={onSubmitHandle}
-        className="flex w-full max-w-md flex-col gap-4 rounded-xl bg-custom-white p-10 shadow-lg"
+    <>
+      <Link
+        to="/"
+        className="flex items-center justify-end gap-2 pt-5 pr-5 text-xl font-semibold text-custom-blue-primary"
       >
-        <div className="mb-2 flex w-full flex-row items-end justify-between">
-          <h1 className="text-3xl font-bold text-custom-black-primary">
-            Masuk
-          </h1>
-          <Link
-            to="/"
-            className="flex items-center gap-2 text-xl font-semibold text-custom-blue-primary"
-          >
-            <FaHome />
-            Ke Beranda
-          </Link>
-          {productId ? (
-            <Link
-              to={`/register?productId=${productId}`}
-              className="text-xl font-semibold text-custom-blue-primary"
-            >
-              Daftar
-            </Link>
-          ) : (
-            <Link
-              to="/register"
-              className="text-xl font-semibold text-custom-blue-primary"
-            >
-              Daftar
-            </Link>
-          )}
-        </div>
-        <div className="flex flex-col gap-2">
-          <label htmlFor="email" className="text-sm">
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            className="rounded-lg border-[1px] border-custom-black-secondary p-3 text-sm text-custom-black-secondary outline-none placeholder:font-light placeholder:text-custom-grey"
-            placeholder="cth: user@fitnest.com"
-            value={loginData.email}
-            onChange={inputChangeHandle}
-          />
-        </div>
-        <div className="flex flex-col gap-2">
-          <label htmlFor="password" className="text-sm">
-            Password
-          </label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            className="rounded-lg border-[1px] border-custom-black-secondary p-3 text-sm text-custom-black-secondary outline-none placeholder:font-light placeholder:text-custom-grey"
-            placeholder="********"
-            value={loginData.password}
-            onChange={inputChangeHandle}
-          />
-        </div>
-        <button
-          type="submit"
-          className="mt-2 rounded-lg bg-custom-blue-primary py-3 text-white transition-all duration-300 ease-linear hover:bg-custom-blue-secondary"
+        <FaHome />
+        Ke Beranda
+      </Link>
+      <div className="flex min-h-screen items-center justify-center bg-custom-white  p-4">
+        <form
+          onSubmit={onSubmitHandle}
+          className="flex w-full max-w-md flex-col gap-4 rounded-xl bg-custom-white p-10 shadow-lg"
         >
-          Masuk
-        </button>
-      </form>
-    </div>
+          <div className="mb-2 flex w-full flex-row items-end justify-between">
+            <h1 className="text-3xl font-bold text-custom-black-primary">
+              Masuk
+            </h1>
+            {productId ? (
+              <Link
+                to={`/register?productId=${productId}`}
+                className="text-xl font-semibold text-custom-blue-primary"
+              >
+                Daftar
+              </Link>
+            ) : (
+              <Link
+                to="/register"
+                className="text-xl font-semibold text-custom-blue-primary"
+              >
+                Daftar
+              </Link>
+            )}
+          </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="email" className="text-sm">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="rounded-lg border-[1px] border-custom-black-secondary p-3 text-sm text-custom-black-secondary outline-none placeholder:font-light placeholder:text-custom-grey"
+              placeholder="cth: user@fitnest.com"
+              value={loginData.email}
+              onChange={inputChangeHandle}
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="password" className="text-sm">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="rounded-lg border-[1px] border-custom-black-secondary p-3 text-sm text-custom-black-secondary outline-none placeholder:font-light placeholder:text-custom-grey"
+              placeholder="********"
+              value={loginData.password}
+              onChange={inputChangeHandle}
+            />
+          </div>
+          <button
+            type="submit"
+            className="mt-2 rounded-lg bg-custom-blue-primary py-3 text-white transition-all duration-300 ease-linear hover:bg-custom-blue-secondary"
+          >
+            Masuk
+          </button>
+        </form>
+      </div>
+    </>
   );
 }
--- src/routes/register.tsx
+++ src/routes/register.tsx
@@ -56,105 +56,107 @@ export default function Register() {
   };
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-custom-white p-4">
-      <form
-        onSubmit={registerHandle}
-        className="flex w-full max-w-md flex-col gap-4 rounded-xl bg-custom-white p-10 shadow-lg"
+    <>
+      <Link
+        to="/"
+        className="flex items-center justify-end gap-2 pt-5 pr-5 text-xl font-semibold text-custom-blue-primary"
       >
-        <div className="flex w-full flex-row items-end justify-between">
-          <h1 className="text-3xl font-bold text-custom-black-primary">
-            Daftar
-          </h1>
-          <Link
-            to="/"
-            className="flex items-center gap-2 text-xl font-semibold text-custom-blue-primary"
-          >
-            <FaHome />
-            Ke Beranda
-          </Link>
-          {productId ? (
-            <Link
-              to={`/login?productId=${productId}`}
-              className="text-xl font-semibold text-custom-blue-primary"
-            >
-              Masuk
-            </Link>
-          ) : (
-            <Link
-              to="/login"
-              className="text-xl font-semibold text-custom-blue-primary"
-            >
-              Masuk
-            </Link>
-          )}
-        </div>
-        <div className="flex flex-col gap-2">
-          <label htmlFor="firstName" className="text-sm">
-            Nama Depan
-          </label>
-          <input
-            type="text"
-            id="firstName"
-            name="firstName"
-            className="rounded-lg border-[1px] border-custom-black-secondary p-3 text-sm text-custom-black-secondary outline-none placeholder:font-light placeholder:text-custom-grey"
-            placeholder="cth: Mesut"
-          />
-        </div>
-        <div className="flex flex-col gap-2">
-          <label htmlFor="lastName" className="text-sm">
-            Nama Belakang
-          </label>
-          <input
-            type="text"
-            id="lastName"
-            name="lastName"
-            className="rounded-lg border-[1px] border-custom-black-secondary p-3 text-sm text-custom-black-secondary outline-none placeholder:font-light placeholder:text-custom-grey"
-            placeholder="cth: Ozil"
-          />
-        </div>
-        <div className="flex flex-col gap-2">
-          <label htmlFor="email" className="text-sm">
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            className="rounded-lg border-[1px] border-custom-black-secondary p-3 text-sm text-custom-black-secondary outline-none placeholder:font-light placeholder:text-custom-grey"
-            placeholder="cth: user@fitnest.com"
-          />
-        </div>
-        <div className="flex flex-col gap-2">
-          <label htmlFor="password" className="text-sm">
-            Password
-          </label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            className="rounded-lg border-[1px] border-custom-black-secondary p-3 text-sm text-custom-black-secondary outline-none placeholder:font-light placeholder:text-custom-grey"
-            placeholder="minimal 8 karakter"
-          />
-        </div>
-        <div className="flex flex-col gap-2">
-          <label htmlFor="confirmPassword" className="text-sm">
-            Konfirmasi Password
-          </label>
-          <input
-            type="password"
-            id="confirmPassword"
-            name="confirmPassword"
-            className="rounded-lg border-[1px] border-custom-black-secondary p-3 text-sm text-custom-black-secondary outline-none placeholder:font-light placeholder:text-custom-grey"
-            placeholder="harus sama dengan password"
-          />
-        </div>
-        <button
-          type="submit"
-          className="rounded-lg bg-custom-blue-primary py-3 text-white transition-all duration-300 ease-linear hover:bg-custom-blue-secondary"
+        <FaHome />
+        Ke Beranda
+      </Link>
+      <div className="flex min-h-screen items-center justify-center bg-custom-white p-4">
+        <form
+          onSubmit={registerHandle}
+          className="flex w-full max-w-md flex-col gap-4 rounded-xl bg-custom-white p-10 shadow-lg"
         >
-          Daftar
-        </button>
-      </form>
-    </div>
+          <div className="flex w-full flex-row items-end justify-between">
+            <h1 className="text-3xl font-bold text-custom-black-primary">
+              Daftar
+            </h1>
+            {productId ? (
+              <Link
+                to={`/login?productId=${productId}`}
+                className="text-xl font-semibold text-custom-blue-primary"
+              >
+                Masuk
+              </Link>
+            ) : (
+              <Link
+                to="/login"
+                className="text-xl font-semibold text-custom-blue-primary"
+              >
+                Masuk
+              </Link>
+            )}
+          </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="firstName" className="text-sm">
+              Nama Depan
+            </label>
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              className="rounded-lg border-[1px] border-custom-black-secondary p-3 text-sm text-custom-black-secondary outline-none placeholder:font-light placeholder:text-custom-grey"
+              placeholder="cth: Mesut"
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="lastName" className="text-sm">
+              Nama Belakang
+            </label>
+            <input
+              type="text"
+              id="lastName"
+              name="lastName"
+              className="rounded-lg border-[1px] border-custom-black-secondary p-3 text-sm text-custom-black-secondary outline-none placeholder:font-light placeholder:text-custom-grey"
+              placeholder="cth: Ozil"
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="email" className="text-sm">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="rounded-lg border-[1px] border-custom-black-secondary p-3 text-sm text-custom-black-secondary outline-none placeholder:font-light placeholder:text-custom-grey"
+              placeholder="cth: user@fitnest.com"
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="password" className="text-sm">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="rounded-lg border-[1px] border-custom-black-secondary p-3 text-sm text-custom-black-secondary outline-none placeholder:font-light placeholder:text-custom-grey"
+              placeholder="minimal 8 karakter"
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="confirmPassword" className="text-sm">
+              Konfirmasi Password
+            </label>
+            <input
+              type="password"
+              id="confirmPassword"
+              name="confirmPassword"
+              className="rounded-lg border-[1px] border-custom-black-secondary p-3 text-sm text-custom-black-secondary outline-none placeholder:font-light placeholder:text-custom-grey"
+              placeholder="harus sama dengan password"
+            />
+          </div>
+          <button
+            type="submit"
+            className="rounded-lg bg-custom-blue-primary py-3 text-white transition-all duration-300 ease-linear hover:bg-custom-blue-secondary"
+          >
+            Daftar
+          </button>
+        </form>
+      </div>
+    </>
   );
 }
